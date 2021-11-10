421b41605e54f23c1aca98d0c3c2642c1cd332f0
seno-b <bangshinho@gmail.com>
2021-11-10 09:03:54 +0900

    feat: 1. IoC 컨테이너와 빈 5부 빈의 스코프

---
+++ src/main/java/me/senob/springframeworkcore/Proto.java
@@ -0,0 +1,8 @@
+package me.senob.springframeworkcore;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component @Scope("prototype")
+public class Proto {
+}
